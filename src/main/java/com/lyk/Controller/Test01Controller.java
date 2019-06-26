84db1a187ad1c667570014c1afa500c07fe74604
liyunkai <18326895933@163.com>
2019-06-26 11:06:54 +0800

    测试鸭02

--- src/main/java/com/lyk/Controller/Test01Controller.java
+++ src/main/java/com/lyk/Controller/Test01Controller.java
@@ -3,6 +3,7 @@ package com.lyk.Controller;
 public class Test01Controller {
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("test001");
     }
 
 }
